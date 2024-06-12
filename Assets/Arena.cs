ee0a33d1f2a58cb457920cd70b30f33190a34610
JuggerN4uti <97904924+JuggerN4uti@users.noreply.github.com>
2024-06-12 14:08:39 +0200

    + większa ilość mobków

--- Assets/Arena.cs
+++ Assets/Arena.cs
@@ -100,7 +100,7 @@ public class Arena : MonoBehaviour
         map.playerStats.NewDay();
         VendingMachineObject.SetActive(true);
         VaningMachineScript.SetCabinet();
-        strength += 60 + strength / 10;
+        strength += 60 + strength / 9;
         time += 2f;
         Invoke("NextRound", 30f);
     }
